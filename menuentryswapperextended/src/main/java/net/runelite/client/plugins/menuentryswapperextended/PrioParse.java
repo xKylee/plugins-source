c16a4c5b5e0437e264137b822d39ad7bcb91daf8
Kyle <48519776+xKylee@users.noreply.github.com>
2020-02-26 16:52:50 +0000

    Update PrioParse.java

--- menuentryswapperextended/src/main/java/net/runelite/client/plugins/menuentryswapperextended/PrioParse.java
+++ menuentryswapperextended/src/main/java/net/runelite/client/plugins/menuentryswapperextended/PrioParse.java
@@ -22,7 +22,7 @@
  * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
  * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
-package net.runelite.client.plugins.menuentryswapper;
+package net.runelite.client.plugins.menuentryswapperextended;
 
 import com.google.common.base.Splitter;
 import java.util.List;
