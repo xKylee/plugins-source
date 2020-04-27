01f462c0dd98d37cfd77bc9643aa3e071f185c9f
Kyle <48519776+xKylee@users.noreply.github.com>
2020-04-27 12:52:10 +0100

    stronghold: add config option

--- strongholdofsecurity/src/main/java/net/runelite/client/plugins/strongholdofsecurity/StrongholdConfig.java
+++ strongholdofsecurity/src/main/java/net/runelite/client/plugins/strongholdofsecurity/StrongholdConfig.java
@@ -1,7 +1,5 @@
-import ProjectVersions.rlVersion
-
 /*
- * Copyright (c) 2019 Owain van Brakel <https://github.com/Owain94>
+ * Copyright (c) 2019, gazivodag <https://github.com/gazivodag>
  * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
@@ -12,7 +10,6 @@ import ProjectVersions.rlVersion
  * 2. Redistributions in binary form must reproduce the above copyright notice,
  *    this list of conditions and the following disclaimer in the documentation
  *    and/or other materials provided with the distribution.
- *
  * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
  * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
@@ -25,33 +22,24 @@ import ProjectVersions.rlVersion
  * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
-version = "0.0.5"
+package net.runelite.client.plugins.strongholdofsecurity;
 
-project.extra["PluginName"] = "Stronghold"
-project.extra["PluginDescription"] = "Highlights the correct answer to Stronghold of Security questions."
+import java.awt.Color;
+import net.runelite.client.config.Config;
+import net.runelite.client.config.ConfigGroup;
+import net.runelite.client.config.ConfigItem;
 
-dependencies {
-    annotationProcessor(Libraries.lombok)
-    annotationProcessor(Libraries.pf4j)
-
-    compileOnly("com.openosrs:runelite-api:$rlVersion")
-    compileOnly("com.openosrs:runelite-client:$rlVersion")
-
-    compileOnly(Libraries.guice)
-    compileOnly(Libraries.lombok)
-    compileOnly(Libraries.pf4j)
-}
-
-tasks {
-    jar {
-        manifest {
-            attributes(mapOf(
-                    "Plugin-Version" to project.version,
-                    "Plugin-Id" to nameToId(project.extra["PluginName"] as String),
-                    "Plugin-Provider" to project.extra["PluginProvider"],
-                    "Plugin-Description" to project.extra["PluginDescription"],
-                    "Plugin-License" to project.extra["PluginLicense"]
-            ))
-        }
-    }
+@ConfigGroup("strongholdofsecurity")
+public interface StrongholdConfig extends Config
+{
+	@ConfigItem(
+		keyName = "highlightedAnswer",
+		name = "Color of Correct Answer",
+		description = "Highlights the correct answer with the chosen color.",
+		position = 0
+	)
+	default Color highlightedAnswer()
+	{
+		return new Color(255, 0, 0);
+	}
 }
