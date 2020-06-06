58049c231c8eb1aaae06185abb8e4592eaa1be29
CJP10 <14205938+CJP10@users.noreply.github.com>
2020-06-06 13:02:44 -0400

    remove graphics object detection (#132)

--- coxhelper/coxhelper.gradle.kts
+++ coxhelper/coxhelper.gradle.kts
@@ -25,7 +25,7 @@ import ProjectVersions.rlVersion
  * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
-version = "0.0.11"
+version = "0.0.12"
 
 project.extra["PluginName"] = "CoX Helper"
 project.extra["PluginDescription"] = "All-in-one plugin for Chambers of Xeric"
