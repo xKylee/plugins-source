a123d77d0f9b5a16b127ca38decf87e6e8409cea
CJP10 <14205938+CJP10@users.noreply.github.com>
2020-06-07 14:14:51 -0400

    Revert "coxhelper: add extra tick" (#138)
    * Revert "coxhelper: add extra tick (#137)"
    
    This reverts commit 7353912977cdc3a482ed52111a8f000608965fac.
    
    * Update coxhelper.gradle.kts

--- coxhelper/coxhelper.gradle.kts
+++ coxhelper/coxhelper.gradle.kts
@@ -25,7 +25,7 @@ import ProjectVersions.rlVersion
  * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
-version = "0.0.16"
+version = "0.0.17"
 
 project.extra["PluginName"] = "CoX Helper"
 project.extra["PluginDescription"] = "All-in-one plugin for Chambers of Xeric"
