db716c243cc124ab57ab080ff4e01113dc15d6a9
Kyle <48519776+xKylee@users.noreply.github.com>
2020-07-04 20:59:54 +0100

    environmentaid: add zammy and snow options (#201)
    environmentaid: add zammy and snow options
    
    Co-authored-by: xKylee <>

--- environmentaid/environmentaid.gradle.kts
+++ environmentaid/environmentaid.gradle.kts
@@ -25,7 +25,7 @@ import ProjectVersions.rlVersion
  * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
-version = "0.0.2"
+version = "0.0.3"
 
 project.extra["PluginName"] = "Environment Aid"
 project.extra["PluginDescription"] = "Environment Aid to assist in certain areas"
