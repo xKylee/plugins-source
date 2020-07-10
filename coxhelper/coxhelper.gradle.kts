33bb983f11a9cfbc71cc22b8abcf046ee16255aa
CJP10 <14205938+CJP10@users.noreply.github.com>
2020-07-10 19:24:06 -0400

    coxhelper: start attack counter earlier (#208)
    * start attack counter earlier
    
    * start attack counter earlier

--- coxhelper/coxhelper.gradle.kts
+++ coxhelper/coxhelper.gradle.kts
@@ -25,7 +25,7 @@ import ProjectVersions.rlVersion
  * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
-version = "0.0.46"
+version = "0.0.47"
 
 project.extra["PluginName"] = "CoX Helper"
 project.extra["PluginDescription"] = "All-in-one plugin for Chambers of Xeric"
