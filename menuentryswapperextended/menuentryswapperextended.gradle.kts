6d69071c623eb609d935c58295fba8a45b92ef51
Kyle <48519776+xKylee@users.noreply.github.com>
2020-07-17 21:52:25 +0100

    Mesextended: ferox enclave support (#214)
    * Update DuelingRingMode.java
    
    * Update menuentryswapperextended.gradle.kts

--- menuentryswapperextended/menuentryswapperextended.gradle.kts
+++ menuentryswapperextended/menuentryswapperextended.gradle.kts
@@ -25,7 +25,7 @@ import ProjectVersions.rlVersion
  * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
-version = "0.0.13"
+version = "0.0.14"
 
 project.extra["PluginName"] = "Menu Entry Swapper Extended"
 project.extra["PluginDescription"] = "Change the default option that is displayed when hovering over objects"
