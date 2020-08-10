ce2396aaf8cc7b80bc5c12ec61badfe451c21aac
grian <54550982+grian32@users.noreply.github.com>
2020-08-10 04:21:08 +0300

    made the description clearer (#222)
    * made the description clearer
    
    added "and what to pray at TzTok-Jad" after the current description to make it a little clearer than it shows what to pray aswell
    
    * bumped version

--- fightcave/src/main/java/net/runelite/client/plugins/fightcave/FightCavePlugin.java
+++ fightcave/src/main/java/net/runelite/client/plugins/fightcave/FightCavePlugin.java
@@ -63,7 +63,7 @@ import org.pf4j.Extension;
 @PluginDescriptor(
 	name = "Fight Cave",
 	enabledByDefault = false,
-	description = "Displays current and upcoming wave monsters in the Fight Caves",
+	description = "Displays current and upcoming wave monsters in the Fight Caves and what to pray at TzTok-Jad",
 	tags = {"bosses", "combat", "minigame", "overlay", "pve", "pvm", "jad", "fire", "cape", "wave"},
 	type = PluginType.PVM
 )
