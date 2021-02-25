1bc0af4614e36cd622c26dc29a10071ff7ea6ddc
Kyle <48519776+xKylee@users.noreply.github.com>
2021-02-25 14:39:27 +0000

    Update Dependencies.kt

--- buildSrc/src/main/kotlin/Dependencies.kt
+++ buildSrc/src/main/kotlin/Dependencies.kt
@@ -24,7 +24,7 @@
  */
 
 object ProjectVersions {
-    const val rlVersion = "3.5.4"
+    const val rlVersion = "4.0.0"
     const val apiVersion = "0.0.1"
     const val kotlinVersion = "1.3.72"
 }
