443929c6e793b20e9b5ada60c5a592062326a209
Kyle <48519776+xKylee@users.noreply.github.com>
2021-02-27 01:01:40 +0000

    add: spawntimer
    add: spawntimer

---
+++ spawntimer/src/main/java/net/runelite/client/plugins/spawntimer/SpawnTimer.java
@@ -0,0 +1,29 @@
+package net.runelite.client.plugins.spawntimer;
+
+import net.runelite.api.NPC;
+
+class SpawnTimer
+{
+	private NPC npc;
+	private int tick;
+
+	public void setNpc(NPC n)
+	{
+		npc = n;
+	}
+
+	NPC getNpc()
+	{
+		return npc;
+	}
+
+	public void setTick(int n)
+	{
+		tick = n;
+	}
+
+	int getTick()
+	{
+		return tick;
+	}
+}
