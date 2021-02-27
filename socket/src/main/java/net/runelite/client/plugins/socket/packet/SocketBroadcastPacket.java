6af7a9d4aecd5e47e53f2b28c4e6347d5e6a1049
Kyle <48519776+xKylee@users.noreply.github.com>
2021-02-27 17:49:26 +0000

    bump

--- socket/src/main/java/net/runelite/client/plugins/socket/packet/SocketBroadcastPacket.java
+++ socket/src/main/java/net/runelite/client/plugins/socket/packet/SocketBroadcastPacket.java
@@ -3,14 +3,13 @@ package net.runelite.client.plugins.socket.packet;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
-import net.runelite.api.events.Event;
 import net.runelite.client.plugins.socket.org.json.JSONObject;
 
 /**
  * Event triggered by alternative plugins, broadcasting a packet to the server.
  */
 @AllArgsConstructor
-public class SocketBroadcastPacket implements Event
+public class SocketBroadcastPacket
 {
 
 	@Getter(AccessLevel.PUBLIC)
