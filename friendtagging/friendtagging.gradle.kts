f27b2d3bbce78e66e808040757b0c08efdb293ad
Kyle <48519776+xKylee@users.noreply.github.com>
2021-02-28 13:13:28 +0000

    project: playerindicators + friend tagging (#321)
    * Update settings.gradle.kts
    
    * Update friendtagging.gradle.kts
    
    * Update playerindicatorsextended.gradle.kts
    
    * Update settings.gradle.kts

--- friendtagging/friendtagging.gradle.kts
+++ friendtagging/friendtagging.gradle.kts
@@ -25,7 +25,7 @@ import ProjectVersions.rlVersion
  * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
-version = "5.0.0"
+version = "5.0.1"
 
 project.extra["PluginName"] = "Friend Tagging"
 project.extra["PluginDescription"] = "Tag people on your friends list"
