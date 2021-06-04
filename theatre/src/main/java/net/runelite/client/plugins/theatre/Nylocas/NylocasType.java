3d74e872c9908da5e9facda11aa9c1d52a310383
BickusDiggus <80493562+BickusDiggus@users.noreply.github.com>
2021-06-04 10:36:20 +0100

    Theatre: Nylocas type hm/sm ids (#401)
    * NylocasType - add hm/sm ids
    
    * bump version

--- theatre/src/main/java/net/runelite/client/plugins/theatre/Nylocas/NylocasType.java
+++ theatre/src/main/java/net/runelite/client/plugins/theatre/Nylocas/NylocasType.java
@@ -11,18 +11,26 @@ import lombok.Getter;
 
 enum NylocasType
 {
-	MELEE_SMALL(8342, 8348),
-	MELEE_BIG(8345, 8351),
-	RANGE_SMALL(8343, 8349),
-	RANGE_BIG(8346, 8352),
-	MAGE_SMALL(8344, 8350),
-	MAGE_BIG(8347, 8353);
+	MELEE_SMALL(8342, 10774, 10791, 8348, 10780, 10797),
+	MELEE_BIG(8345, 10777, 10794, 8351, 10783, 10800),
+	RANGE_SMALL(8343, 10775, 10792, 8349, 10781, 10798),
+	RANGE_BIG(8346, 10778, 10795, 8352, 10784, 10801),
+	MAGE_SMALL(8344, 10776, 10793, 8350, 10782, 10799),
+	MAGE_BIG(8347, 10779, 10796, 8353, 10785, 10802);
 
 	@Getter
 	private int id;
+	@Getter
+	private int id_sm;
+	@Getter
+	private int id_hm;
 
 	@Getter
 	private int aggroId;
+	@Getter
+	private int aggroId_sm;
+	@Getter
+	private int aggroId_hm;
 
 	@Getter
 	private static final HashMap<Integer, NylocasType> lookupMap;
@@ -34,13 +42,21 @@ enum NylocasType
 		for (NylocasType v : NylocasType.values())
 		{
 			lookupMap.put(v.getId(), v);
+			lookupMap.put(v.getId_sm(), v);
+			lookupMap.put(v.getId_hm(), v);
 			lookupMap.put(v.getAggroId(), v);
+			lookupMap.put(v.getAggroId_sm(), v);
+			lookupMap.put(v.getAggroId_hm(), v);
 		}
 	}
 
-	NylocasType(int id, int aggroId)
+	NylocasType(int id, int id_sm, int id_hm, int aggroId, int aggroId_sm, int aggroId_hm)
 	{
 		this.id = id;
+		this.id_sm = id_sm;
+		this.id_hm = id_hm;
 		this.aggroId = aggroId;
+		this.aggroId_sm = aggroId_sm;
+		this.aggroId_hm = aggroId_hm;
 	}
 }
--- theatre/theatre.gradle.kts
+++ theatre/theatre.gradle.kts
@@ -25,7 +25,7 @@ import ProjectVersions.rlVersion
  * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
-version = "5.1.3"
+version = "5.1.4"
 
 project.extra["PluginName"] = "Theatre Of Blood"
 project.extra["PluginDescription"] = "All-in-one plugin for Theatre of Blood"
