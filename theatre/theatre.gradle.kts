fff238bc5f9d61be54a32fa3f1351922c247bceb
BickusDiggus <noidied69@gmail.com>
2021-09-09 15:49:44 +0100

    bump version

--- theatre/theatre.gradle.kts
+++ theatre/theatre.gradle.kts
@@ -25,7 +25,7 @@ import ProjectVersions.rlVersion
  * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
-version = "5.1.6"
+version = "5.1.7"
 
 project.extra["PluginName"] = "Theatre Of Blood"
 project.extra["PluginDescription"] = "All-in-one plugin for Theatre of Blood"
