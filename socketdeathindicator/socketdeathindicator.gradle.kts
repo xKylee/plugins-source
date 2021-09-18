fbf4791b812edab31237191d533b24a48015a284
BickusDiggus <80493562+BickusDiggus@users.noreply.github.com>
2021-09-18 20:11:08 +0100

    socketdeathindicators: 200m xp + possible maiden fix (#441)
    * fix 200m + maiden
    
    * bump rlVersion

--- socketdeathindicator/socketdeathindicator.gradle.kts
+++ socketdeathindicator/socketdeathindicator.gradle.kts
@@ -25,7 +25,7 @@ import ProjectVersions.rlVersion
  * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
-version = "5.0.5"
+version = "5.0.6"
 
 project.extra["PluginName"] = "Socket Death Indicator"
 project.extra["PluginDescription"] = "Socket extension for extending removing dead nylocas during Theatre"
