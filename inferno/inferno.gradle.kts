7b3d19425c4f667588e74f8ed66eac65168258b2
Chris Burgess <9002722+cgburgess@users.noreply.github.com>
2021-09-26 12:09:47 -0400

    fix central nibbler calculation (#445)

--- inferno/inferno.gradle.kts
+++ inferno/inferno.gradle.kts
@@ -25,7 +25,7 @@ import ProjectVersions.rlVersion
  * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
-version = "5.0.3"
+version = "5.0.4"
 
 project.extra["PluginName"] = "Inferno"
 project.extra["PluginDescription"] = "Inferno helper"
