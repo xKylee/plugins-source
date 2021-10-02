258c729b9bcfa4ce44ca368a606189bedc523fe2
BickusDiggus <80493562+BickusDiggus@users.noreply.github.com>
2021-10-02 02:23:46 +0100

    mesx: fix loadSwaps (#454)

--- menuentryswapperextended/menuentryswapperextended.gradle.kts
+++ menuentryswapperextended/menuentryswapperextended.gradle.kts
@@ -25,7 +25,7 @@ import ProjectVersions.rlVersion
  * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
-version = "5.1.0"
+version = "5.1.1"
 
 project.extra["PluginName"] = "Menu Entry Swapper Extended"
 project.extra["PluginDescription"] = "Change the default option that is displayed when hovering over objects"
