e8155a7256eaa8764f560d362516e3a349c4b409
Im2be <simon144_14@msn.com>
2021-10-05 21:19:51 +0200

    Added NpcIndicatorsExtended
    This plugin will highlight the true tiles of the NPCs that have been highlighted using the NPC Indicators-plugin.
    The highlighted color from 'NPC Indicators' is respected.
    
    https://imgur.com/a/3p1LiV4

---
+++ npcindicatorsextended/src/main/java/net/runelite/client/plugins/npcindicatorsextended/NpcIndicatorsExtendedOverlay.java
@@ -0,0 +1,140 @@
+/*
+ * Copyright (c) 2018, James Swindle <wilingua@gmail.com>
+ * Copyright (c) 2018, Adam <Adam@sigterm.info>
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright notice, this
+ *    list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR
+ * ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+package net.runelite.client.plugins.npcindicatorsextended;
+
+import java.awt.BasicStroke;
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.Graphics2D;
+import java.awt.Polygon;
+import java.util.List;
+import javax.inject.Inject;
+import net.runelite.api.Client;
+import net.runelite.api.NPC;
+import net.runelite.api.NPCComposition;
+import net.runelite.api.Perspective;
+import net.runelite.api.coords.LocalPoint;
+import net.runelite.api.coords.WorldArea;
+import net.runelite.api.coords.WorldPoint;
+import net.runelite.client.ui.overlay.Overlay;
+import net.runelite.client.ui.overlay.OverlayLayer;
+import net.runelite.client.ui.overlay.OverlayPosition;
+import net.runelite.client.plugins.npchighlight.NpcIndicatorsPlugin;
+import net.runelite.client.plugins.npchighlight.NpcIndicatorsConfig;
+
+public class NpcIndicatorsExtendedOverlay extends Overlay
+{
+	private static final Color TRANSPARENT = new Color(0, 0, 0, 0);
+
+	// Anything but white text is quite hard to see since it is drawn on
+	// a dark background
+	private static final Color TEXT_COLOR = Color.WHITE;
+
+	private final Client client;
+	private final NpcIndicatorsExtendedPlugin plugin;
+	private final NpcIndicatorsPlugin basePlugin;
+	private final NpcIndicatorsExtendedConfig config;
+	private final NpcIndicatorsConfig baseConfig;
+	@Inject
+	NpcIndicatorsExtendedOverlay(final Client client, final NpcIndicatorsExtendedPlugin plugin, final NpcIndicatorsExtendedConfig config)
+	{
+		this.client = client;
+		this.plugin = plugin;
+		this.basePlugin = plugin.getBasePlugin();
+		this.config = config;
+		this.baseConfig = plugin.getBaseConfig();
+		setPosition(OverlayPosition.DYNAMIC);
+		setLayer(OverlayLayer.ABOVE_SCENE);
+	}
+
+	@Override
+	public Dimension render(Graphics2D graphics)
+	{
+		if (!config.trueTile())
+		{
+			return null;
+		}
+
+		for (NPC npc : plugin.getHighlightedNpcs())
+		{
+			renderNpcOverlay(graphics, npc, baseConfig.highlightColor());
+		}
+		return null;
+	}
+
+
+
+	private void renderNpcOverlay(Graphics2D graphics, NPC actor, Color color)
+	{
+		int size = 1;
+		NPCComposition composition = actor.getTransformedComposition();
+
+		if (composition != null)
+		{
+			size = composition.getSize();
+		}
+
+		final WorldPoint wp = actor.getWorldLocation();
+		final Color squareColor = color;
+
+		getSquare(wp, size).forEach(square -> drawTile(graphics, square, squareColor, 1, 255, 50));
+	}
+
+	private List<WorldPoint> getSquare(WorldPoint npcLoc, int npcSize)
+	{
+		return new WorldArea(npcLoc.getX(), npcLoc.getY(), npcSize, npcSize, npcLoc.getPlane()).toWorldPointList();
+	}
+
+	private void drawTile(Graphics2D graphics, WorldPoint point, Color color, int strokeWidth, int outlineAlpha, int fillAlpha)
+	{
+		WorldPoint playerLocation = client.getLocalPlayer().getWorldLocation();
+
+		if (point.distanceTo(playerLocation) >= 32)
+		{
+			return;
+		}
+
+		LocalPoint lp = LocalPoint.fromWorld(client, point);
+
+		if (lp == null)
+		{
+			return;
+		}
+
+		Polygon poly = Perspective.getCanvasTilePoly(client, lp);
+
+		if (poly == null)
+		{
+			return;
+		}
+
+		graphics.setColor(new Color(color.getRed(), color.getGreen(), color.getBlue(), outlineAlpha));
+		graphics.setStroke(new BasicStroke(strokeWidth));
+		graphics.draw(poly);
+		graphics.setColor(new Color(color.getRed(), color.getGreen(), color.getBlue(), fillAlpha));
+		graphics.fill(poly);
+	}
+}
---
+++ npcindicatorsextended/src/main/java/net/runelite/client/plugins/npcindicatorsextended/NpcIndicatorsExtendedPlugin.java
@@ -0,0 +1,192 @@
+package net.runelite.client.plugins.npcindicatorsextended;
+
+import com.google.inject.Provides;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.List;
+import java.util.ArrayList;
+import java.util.Collections;
+import javax.inject.Inject;
+import lombok.extern.slf4j.Slf4j;
+import net.runelite.api.Client;
+import net.runelite.api.GameState;
+import net.runelite.api.NPC;
+import net.runelite.api.events.GameStateChanged;
+import net.runelite.api.util.Text;
+import net.runelite.api.events.NpcDespawned;
+import net.runelite.client.callback.ClientThread;
+import net.runelite.client.config.ConfigManager;
+import net.runelite.client.eventbus.Subscribe;
+import net.runelite.client.plugins.Plugin;
+import net.runelite.client.plugins.PluginDependency;
+import net.runelite.client.plugins.PluginDescriptor;
+import net.runelite.client.plugins.PluginManager;
+import net.runelite.client.plugins.npchighlight.NpcIndicatorsPlugin;
+import net.runelite.client.plugins.npchighlight.NpcIndicatorsConfig;
+import net.runelite.client.ui.overlay.OverlayManager;
+import net.runelite.client.events.ConfigChanged;
+import net.runelite.client.util.WildcardMatcher;
+import lombok.AccessLevel;
+import lombok.Getter;
+import org.pf4j.Extension;
+
+@Extension
+@PluginDescriptor(
+	name = "NPC Indicators Extended",
+	description = "Highlights the true tiles of NPCs that are highlyted by the NPC Indicators-plugin",
+	tags = {"highlight", "minimap", "npcs", "overlay", "respawn", "tags", "true", "tile"}
+)
+
+@Slf4j
+@PluginDependency(NpcIndicatorsPlugin.class)
+public class NpcIndicatorsExtendedPlugin extends Plugin
+{
+
+	@Inject
+	private Client client;
+
+	@Inject
+	private PluginManager pluginManager;
+
+	@Inject
+	private NpcIndicatorsExtendedConfig config;
+
+	@Inject
+	private NpcIndicatorsPlugin NpcIndicatorsPlugin;
+
+	@Inject
+	private NpcIndicatorsConfig NpcIndicatorsConfig;
+
+	@Inject
+	private OverlayManager overlayManager;
+
+	@Inject
+	private NpcIndicatorsExtendedOverlay npcIndicatorsExtendedOverlay;
+
+	@Inject
+	private ClientThread clientThread;
+
+	/**
+	 * NPCs to highlight
+	 */
+	@Getter(AccessLevel.PACKAGE)
+	private final Set<NPC> highlightedNpcs = new HashSet<>();
+
+	/**
+	 * Highlight strings from the configuration
+	 */
+	private List<String> highlights = new ArrayList<>();
+
+	@Provides
+	NpcIndicatorsExtendedConfig provideConfig(ConfigManager configManager)
+	{
+		return configManager.getConfig(NpcIndicatorsExtendedConfig.class);
+	}
+
+	@Override
+	protected void startUp()
+	{
+		overlayManager.add(npcIndicatorsExtendedOverlay);
+		clientThread.invoke(() ->
+		{
+			rebuildAllNpcs();
+		});
+	}
+
+	@Override
+	protected void shutDown()
+	{
+		overlayManager.remove(npcIndicatorsExtendedOverlay);
+		clientThread.invoke(() ->
+		{
+			highlightedNpcs.clear();
+		});
+	}
+
+	@Subscribe
+	private void onConfigChanged(ConfigChanged configChanged)
+	{
+		if (configChanged.getGroup().indexOf("npcindicators") != 0)
+		{
+			return;
+		}
+
+		highlights = getHighlights();
+		rebuildAllNpcs();
+	}
+
+	private List<String> getHighlights()
+	{
+		final String configNpcs = NpcIndicatorsConfig.getNpcToHighlight().toLowerCase();
+
+		if (configNpcs.isEmpty())
+		{
+			return Collections.emptyList();
+		}
+
+		return Text.fromCSV(configNpcs);
+	}
+
+	@Subscribe
+	private void onGameStateChanged(GameStateChanged event)
+	{
+		if (event.getGameState() == GameState.LOGIN_SCREEN ||
+				event.getGameState() == GameState.HOPPING)
+		{
+			highlightedNpcs.clear();
+		}
+	}
+	@Subscribe
+	private void onNpcDespawned(NpcDespawned npcDespawned)
+	{
+		final NPC npc = npcDespawned.getNpc();
+		highlightedNpcs.remove(npc);
+	}
+
+	private void highlightNpcIfMatch(final NPC npc)
+	{
+		final String npcName = npc.getName();
+		if (npcName != null)
+		{
+			for (String highlight : highlights)
+			{
+				if (WildcardMatcher.matches(highlight, npcName))
+				{
+					highlightedNpcs.add(npc);
+					return;
+				}
+			}
+		}
+
+		highlightedNpcs.remove(npc);
+	}
+
+
+	private void rebuildAllNpcs()
+	{
+		highlightedNpcs.clear();
+
+		if (client.getGameState() != GameState.LOGGED_IN &&
+				client.getGameState() != GameState.LOADING)
+		{
+			// NPCs are still in the client after logging out,
+			// but we don't want to highlight those.
+			return;
+		}
+
+		for (NPC npc : client.getNpcs())
+		{
+			highlightNpcIfMatch(npc);
+		}
+	}
+
+	public NpcIndicatorsPlugin getBasePlugin()
+	{
+		return NpcIndicatorsPlugin;
+	}
+
+	public NpcIndicatorsConfig getBaseConfig()
+	{
+		return NpcIndicatorsConfig;
+	}
+}
