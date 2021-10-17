f1c502ad6ac15eeada6b94a9d2be8f16eb8c9f8d
Kyle <48519776+xKylee@users.noreply.github.com>
2021-10-17 01:08:52 +0100

    bump

--- buildSrc/src/main/kotlin/Dependencies.kt
+++ buildSrc/src/main/kotlin/Dependencies.kt
@@ -24,7 +24,7 @@
  */
 
 object ProjectVersions {
-    const val rlVersion = "4.12.0"
+    const val rlVersion = "4.12.1"
     const val apiVersion = "^1.0.0"
     const val kotlinVersion = "1.3.72"
 }
