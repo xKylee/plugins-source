6628a65b188ab663d6d164203bab85147adee84b
Owain van Brakel <owain.vanbrakel@gmail.com>
2021-11-03 14:13:35 +0100

    Update Dependencies.kt

--- buildSrc/src/main/kotlin/Dependencies.kt
+++ buildSrc/src/main/kotlin/Dependencies.kt
@@ -24,7 +24,7 @@
  */
 
 object ProjectVersions {
-    const val rlVersion = "4.12.2"
+    const val rlVersion = "4.15.2"
     const val apiVersion = "^1.0.0"
     const val kotlinVersion = "1.3.72"
 }
