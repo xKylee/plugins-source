012f6479a05566ea85744e06d5debb739c779959
Chris Burgess <9002722+cgburgess@users.noreply.github.com>
2021-12-02 11:48:08 -0500

    Revert "Update betterequipmentinspector.gradle.kts"
    This reverts commit 6750c290dadc15f2a5ee12eefb2a49cfab10de30.

--- betterequipmentinspector/betterequipmentinspector.gradle.kts
+++ betterequipmentinspector/betterequipmentinspector.gradle.kts
@@ -25,7 +25,7 @@ import ProjectVersions.rlVersion
  * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
-version = "5.0.7"
+version = "5.0.6"
 
 project.extra["PluginName"] = "Better Equipment Inspector"
 project.extra["PluginDescription"] = "Inspect other players their gear"
