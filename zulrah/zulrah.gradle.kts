a5d80e9e9eef6e1e91b779912b77415483f1e052
Chris Burgess <9002722+cgburgess@users.noreply.github.com>
2021-12-13 06:48:12 -0500

    zulrah: fix jad phase prayer overlay (#500)
    * fix jad phase prayers
    
    * Update Dependencies.kt

--- zulrah/zulrah.gradle.kts
+++ zulrah/zulrah.gradle.kts
@@ -25,7 +25,7 @@ import ProjectVersions.rlVersion
  * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
-version = "5.0.2"
+version = "5.0.3"
 
 project.extra["PluginName"] = "Zulrah"
 project.extra["PluginDescription"] = "Shows tiles on where to stand during the phases and what prayer to use"
