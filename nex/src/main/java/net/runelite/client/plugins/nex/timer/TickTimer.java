bc7e7bc1ab2ab2ee0e3dee405ee5e766af6106b5
Chris Burgess <9002722+cgburgess@users.noreply.github.com>
2022-01-10 18:21:49 -0500

    rework tick timers

---
+++ nex/src/main/java/net/runelite/client/plugins/nex/timer/TickTimer.java
@@ -0,0 +1,63 @@
+package net.runelite.client.plugins.nex.timer;
+
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
+
+@EqualsAndHashCode
+public class TickTimer
+{
+	@Getter
+	private int ticks = 0;
+
+	private final Action action;
+
+	public TickTimer(Action action)
+	{
+		this.action = action;
+	}
+
+	public TickTimer()
+	{
+		this.action = null;
+	}
+
+	public void setTicks(int ticks)
+	{
+		this.ticks = Math.max(0, ticks);
+	}
+
+	public void tick()
+	{
+		if (ticks > 0)
+		{
+			ticks -= 1;
+
+			if (ticks == 0 && action != null)
+			{
+				action.method();
+			}
+		}
+	}
+
+	public void reset()
+	{
+		setTicks(0);
+	}
+
+	public boolean isActive()
+	{
+		return getTicks() > 0;
+	}
+
+	public boolean isExpired()
+	{
+		return !isActive();
+	}
+
+	@Override
+	public String toString()
+	{
+		return String.valueOf(ticks);
+	}
+}
