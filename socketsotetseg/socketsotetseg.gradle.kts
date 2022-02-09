d2dd3be70b6d7ed2da715a2fd2a3485da7d0bbbf
JumpIfZero <defchinhunter@gmail.com>
2022-02-09 16:02:13 +0200

    project: recompile for new rev

--- socketsotetseg/socketsotetseg.gradle.kts
+++ socketsotetseg/socketsotetseg.gradle.kts
@@ -25,7 +25,7 @@ import ProjectVersions.rlVersion
  * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
-version = "5.0.6"
+version = "5.0.7"
 
 project.extra["PluginName"] = "Socket Sotetseg"
 project.extra["PluginDescription"] = "Extended plugin handler for Sotetseg in the Theatre of Blood."
