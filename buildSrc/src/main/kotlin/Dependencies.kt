4e50af3e5eb4ff916d4de514f14b1a34f9400e60
Owain van Brakel <owain.vanbrakel@gmail.com>
2022-04-14 04:51:38 +0200

    project: Bump

--- buildSrc/src/main/kotlin/Dependencies.kt
+++ buildSrc/src/main/kotlin/Dependencies.kt
@@ -24,7 +24,7 @@
  */
 
 object ProjectVersions {
-    const val rlVersion = "4.20.2"
+    const val rlVersion = "4.23.0"
     const val apiVersion = "^1.0.0"
     const val kotlinVersion = "1.3.72"
 }
--- wildernesslocations/wildernesslocations.gradle.kts
+++ wildernesslocations/wildernesslocations.gradle.kts
@@ -25,7 +25,7 @@ import ProjectVersions.rlVersion
  * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
-version = "1.0.4"
+version = "1.0.5"
 
 project.extra["PluginName"] = "Wilderness Locations"
 project.extra["PluginDescription"] = "Indicates the players current location in the wild"
